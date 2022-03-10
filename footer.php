faf3b3d4c13c9295f3b4591f494b758406fe9012
Rogério J. Barbosa <52040684+metalmaniack@users.noreply.github.com>
2022-03-10 20:14:50 -0300

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-<center>
-    <footer class="bottom">
-        <div class="container">
-            <span class="text-muted">Rogerio J. Barbosa 2022. Todos os direitos reservados.</span>
-        </div>
-    </footer>
-</center>
